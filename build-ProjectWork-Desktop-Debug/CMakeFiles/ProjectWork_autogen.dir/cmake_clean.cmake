a480399d44793d341f7b2d712cc2a8122ccfb0b1
leonardo <leonardo.casar8@gmail.com>
2023-05-15 20:50:55 +0200

    added main project

---
+++ build-ProjectWork-Desktop-Debug/CMakeFiles/ProjectWork_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectWork_autogen"
+  "ProjectWork_autogen/include/ui_mainwindow.h"
+  "ProjectWork_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ProjectWork_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
