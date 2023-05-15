6bb2a6890f4955561256e97441bd5d35c480c9f8
LeonardoCasarotto <61588372+LeonardoCasarotto@users.noreply.github.com>
2023-05-15 20:46:41 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/PwTpsit_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PwTpsit_autogen"
-  "PwTpsit_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PwTpsit_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
