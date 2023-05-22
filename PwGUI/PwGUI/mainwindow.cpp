51f3234d985919272b33e36e151c07495399d337
Leonardo <leonardo.casar8@gmail.com>
2023-05-22 17:59:47 +0200

    changed project structure

--- PwGUI/PwGUI/mainwindow.cpp
+++ PwGUI/PwGUI/mainwindow.cpp
@@ -42,32 +42,32 @@ void MainWindow::displayDirectory(const QString& newText)
         QPixmap img;
         if(v.suffix() =="txt"||v.suffix()=="rtf"||v.suffix()=="docx"||v.suffix()=="doc"||v.suffix()=="log"||v.suffix()=="odt"||v.suffix()=="csv"||v.suffix()=="tsv"||v.suffix()=="md"||v.suffix()=="json"){
 
-           img.load("../dependencies/icons/documentFile.png");
+           img.load("../PwGUI/dependencies/icons/documentFile.png");
         }
 
         else if(v.suffix()=="png"||v.suffix()=="ico"||v.suffix()=="svg"||v.suffix()=="raw"||v.suffix()=="gif"||v.suffix()=="jpg"||v.suffix()=="jpeg"||v.suffix()=="heic"||v.suffix()=="heif"||v.suffix()=="webp"){
-           img.load("../dependencies/icons/image.png");
+           img.load("../PwGUI/dependencies/icons/image.png");
         }
         else if(v.suffix()=="cpp"||v.suffix()=="h"||v.suffix()=="hpp"||v.suffix()=="c"||v.suffix()=="js"||v.suffix()=="html"||v.suffix()=="rb"||v.suffix()=="css"||v.suffix()=="cs"||v.suffix()=="php"||v.suffix()=="java"||v.suffix()=="asm"||v.suffix()=="sh"||v.suffix()=="bat"){
-           img.load("../dependencies/icons/codeFile.png");
+           img.load("../PwGUI/dependencies/icons/codeFile.png");
         }
         else if(v.suffix()=="zip"||v.suffix()=="tar"||v.suffix()=="gz"||v.suffix()=="7z"){
-           img.load("../dependencies/icons/compressedFolder.png");
+           img.load("../PwGUI/dependencies/icons/compressedFolder.png");
         }
         else if(v.suffix()=="pdf"){
-           img.load("../dependencies/icons/pdfFile.png");
+           img.load("../PwGUI/dependencies/icons/pdfFile.png");
         }
         else if(v.isDir()){
             QDir folderDir(v.absoluteFilePath());
             if(folderDir.isEmpty()){
-                img.load("../dependencies/icons/emptyFolder.png");
+                img.load("../PwGUI/dependencies/icons/emptyFolder.png");
             }
             else{
-                img.load("../dependencies/icons/fullFolder.png");
+                img.load("../PwGUI/dependencies/icons/fullFolder.png");
             }
         }
         else{
-            img.load("../dependencies/icons/genericFile.png");
+            img.load("../PwGUI/dependencies/icons/genericFile.png");
         }
 
 
@@ -98,9 +98,6 @@ void MainWindow::on_actionper_nome_triggered()
     bool includeSubfolders;
 
     bool ok = SearchInputDialog::getCheckboxValue(nullptr, 0, fileName, includeSubfolders);
-
-
-
 
 }
 
